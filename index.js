17302ca443c514af770483b1e5ea952f575a41d1
mattbechtel1 <bechma02@gmail.com>
2019-12-18 23:23:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return "This Saturday, I want to " + activity + "!";
+}
+
+let mondayWork = function(activity="go to the office") {
+    return "This Monday, I will " + activity + "." 
+}
+
+function wrapAdjective(flair="*") {
+    return function(adj="special") {
+        return "You are " + flair + adj + flair + '!'
+    }
+}
+
+let Calculator = {
+    add: function(addend1, addend2) {
+        return addend1 + addend2
+    },
+    subtract: function(minuend, subtrahend) {
+        return minuend - subtrahend
+    },
+    multiply: function(multplicand, multiplier) {
+        return multplicand * multiplier
+    },
+    divide: function(dividend, divisor) {
+        return dividend / divisor
+    }
+}
+
+let actionApplyer = function(startInt, callbacks) {
+    return callbacks.reduce((accumulator, func) => func(accumulator), startInt);
+    }
